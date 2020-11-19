3ed93c3f49145656f60d26d578862a3172fba128
gergopistai <gergopistai@gmail.com>
2020-11-19 13:15:35 +0100

    finished chapter 9

--- chapter 9/run-all.sh
+++ chapter 9/run-all.sh
@@ -11,3 +11,6 @@ g++ dates-9.4.3.cpp -o dates-9.4.3.out
 echo "Version 9.7.1"
 g++ dates-9.7.1.cpp -o dates-9.7.1.out
 ./dates-9.7.1.out
+echo "Version 9.7.4"
+g++ dates-9.7.4.cpp -o dates-9.7.4.out
+./dates-9.7.4.out
